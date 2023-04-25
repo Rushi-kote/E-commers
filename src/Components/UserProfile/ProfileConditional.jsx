6718a2df4386388c1504df634b2e967a15076074
Rushi-kote <Koterushi@gmail.com>
2023-04-25 10:15:59 +0530

    Added additional functionalities

--- src/Components/UserProfile/ProfileConditional.jsx
+++ src/Components/UserProfile/ProfileConditional.jsx
@@ -13,6 +13,7 @@ const ProfileConditional = () => {
 
     const onClickSignOut = () =>{
       SetUser({status:false});
+      localStorage.removeItem("Token");
       navigate('/SignIn');
     }
 
